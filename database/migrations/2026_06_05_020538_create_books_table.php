a8b75a9061091dcf2af3028d0c38b1bebbda4ea4
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-06-05 13:33:20 +0700

    create database migration

---
+++ database/migrations/2026_06_05_020538_create_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id('id_book');
+            $table->string('title', 100);
+            $table->string('author', 100);
+            $table->string('publisher', 100);
+            $table->string('category', 50);
+            $table->string('year', 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
