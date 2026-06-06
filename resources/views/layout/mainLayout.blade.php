827dff3d1be8b77b8b985559adda4e2dbfa8c53a
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-06-06 07:50:03 +0700

    feat: added home page and the route

--- resources/views/layout/mainLayout.blade.php
+++ resources/views/layout/mainLayout.blade.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>E-Library</title>
 </head>
 <body>
     @include('layout.header')
+
+    <div class="container">
+        @include('partial.home')
+    </div>
 
     @include('layout.footer')
 </body>
