02600471b0926e6f45944be8295b4d1f6d0d81be
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-06-06 16:18:03 +0700

    feat: created book model and refactor the view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,8 +10,13 @@ class HomeController extends Controller
 
     public function index(){
         $page = 'partial.home';
+        $books = Book::all();
+        $no = 1;
+
         return view('index', [
-            'page' => $page
+            'page' => $page,
+            'books' => $books,
+            'no' => $no
         ]);
     }
 
--- resources/views/partial/home.blade.php
+++ resources/views/partial/home.blade.php
@@ -19,15 +19,32 @@
                     <th>No</th>
                     <th>Book Title</th>
                     <th>Author</th>
+                    <th>Publisher</th>
                     <th>Year</th>
                     <th>Cataogry</th>
                     <th>Action</th>
                 </tr>
             </thead>
             <tbody>
+                @forelse ($books as $book)
+                <tr>
+                    <td>{{ $no }}</td>
+                    <td>{{ $book->title }}</td>
+                    <td>{{ $book->author }}</td>
+                    <td>{{ $book->publisher }}</td>
+                    <td>{{ $book->year }}</td>
+                    <td><span class="badge">{{ $book->category }}</span></td>
+                    <td>
+                        <a href="{{ route('edit') }}" class="edit-btn">Edit</a>
+                        <a href="#" class="delete-btn">Delete</a>
+                    </td>
+                </tr>
+                <?php $no++;?>
+                @empty
                 <tr>
                     <td colspan="6" style="text-align: center">No books are available yet</td>
                 </tr>
+                @endforelse
             </tbody>
     </table>
 </div>
